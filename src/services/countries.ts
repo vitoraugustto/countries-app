d04effb9dfd1c77189083d537b07728cd4cfdd95
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-07 18:42:56 -0300

    feat: create service fetchCountry

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -3,3 +3,7 @@ import { instance } from "./axios";
 export const fetchCountries = () => {
   return instance("/v3.1/all");
 };
+
+export const fetchCountry = (name: string) => {
+  return instance(`/v3.1/name/${name}`);
+};
