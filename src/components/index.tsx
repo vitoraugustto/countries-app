8c25d1d5daf4657f6b9b54bbb8d3fa63a572a783
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-08 12:47:58 -0300

    feat: create Text component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from './Layout/Box';
+import { Text } from './Typography/Text';
 import { Button } from './UI/Button';
 import { Input } from './UI/Input';
 
-export { Box, Button, Input };
+export { Box, Button, Input, Text };
