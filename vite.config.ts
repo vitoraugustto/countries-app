729082de9aad871ecf529488f1804404bd28726e
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-08 17:17:50 -0300

    feat: create alias to screens folder

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@common': path.resolve(__dirname, './src/common/'),
       '@theme': path.resolve(__dirname, './src/theme'),
+      '@screens': path.resolve(__dirname, './src/screens/'),
     },
   },
 });
