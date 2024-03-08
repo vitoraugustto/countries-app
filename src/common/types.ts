b35036ff6d0327f1f77177d333a8f67841f9a857
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-08 02:18:45 -0300

    feat: create ICountry interface

--- src/common/types.ts
+++ src/common/types.ts
@@ -5,3 +5,23 @@ type HEX = `#${string}`;
 export type Color = RGB | RGBA | HEX;
 
 export type Status = 'succeeded' | 'failed' | 'idle' | 'pending';
+
+export interface ICountry {
+  altSpellings: string[];
+  capital: string;
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  flag: string;
+  flags: {
+    alt: string;
+    png: string;
+    svg: string;
+  };
+  languages: { [key: string]: string };
+  name: { common: string };
+  population: number;
+}
