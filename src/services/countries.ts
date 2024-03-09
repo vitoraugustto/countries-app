c6f8c662f314ee3c31b47a20daafe5e95bb9a96a
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 01:43:21 -0300

    feat: create service generateCountryCuriosities

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,8 +1,10 @@
 import { AxiosPromise } from 'axios';
 
 import { ICountry } from '@common/types';
+import { OPENAI_CONTEXT, OPENAI_REPLACE_KEY } from '@config/api';
 
 import { instance } from './axios';
+import { chatCompletion } from './openai';
 
 export const fetchCountries = () => {
   return instance('/v3.1/all');
@@ -13,4 +15,15 @@ export const fetchCountry = (
   exact: boolean = false,
 ): AxiosPromise<ICountry[]> => {
   return instance(`/v3.1/name/${name}?fullText=${exact}`);
+};
+
+export const generateCountryCuriosities = (
+  countryName: ICountry['name']['common'],
+) => {
+  return chatCompletion([
+    {
+      role: 'system',
+      content: OPENAI_CONTEXT.replace(OPENAI_REPLACE_KEY, countryName),
+    },
+  ]);
 };
