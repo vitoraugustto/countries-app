cb8ec1a1c4cad3b3b59e10c182d6ce2a7241e10f
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 02:10:56 -0300

    feat: handle AI status

--- src/screens/CountryScreen.tsx
+++ src/screens/CountryScreen.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { ICountry } from '@common/types';
+import { ICountry, Status } from '@common/types';
 import {
   decode,
   formatCapitals,
@@ -9,28 +9,60 @@ import {
   formatLanguages,
 } from '@common/utils';
 import { Box, Text } from '@components';
-import { Divider } from '@mui/material';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { Chip, CircularProgress, Divider } from '@mui/material';
 import { fetchCountry, generateCountryCuriosities } from '@services/countries';
 
 export const CountryScreen = () => {
   const location = useParams() as { countryName: string };
   const [country, setCountry] = useState<ICountry>();
+  const [completionStatus, setCompletionStatus] = useState<Status>('idle');
+  const [countryStatus, setCountryStatus] = useState<Status>('idle');
+
   const [countryCuriosities, setCountryCuriosities] = useState<string | null>(
     '',
   );
 
+  const handleFetchCountry = () => {
+    setCountryStatus('pending');
+
+    fetchCountry(decode(location.countryName), true)
+      .then((res) => {
+        setCountryStatus('succeeded');
+        setCountry(res.data[0]);
+      })
+      .catch(() => setCountryStatus('failed'));
+  };
+
+  const handleGenerateCountryCuriosities = () => {
+    if (country) {
+      setCompletionStatus('pending');
+      generateCountryCuriosities(country.name.common)
+        .then((res) => {
+          setCompletionStatus('succeeded');
+          setCountryCuriosities(res.choices[0].message.content);
+        })
+        .catch(() => setCompletionStatus('failed'));
+    }
+  };
+
+  const handleCuriosities = () => {
+    switch (completionStatus) {
+      case 'failed':
+        return 'Falha ao gerar curiosidades. Por favor, tente novamente mais tarde.';
+      case 'succeeded':
+        return countryCuriosities;
+      default:
+        return '...';
+    }
+  };
+
   useEffect(() => {
-    fetchCountry(decode(location.countryName), true).then((res) =>
-      setCountry(res.data[0]),
-    );
+    handleFetchCountry();
   }, []);
 
   useEffect(() => {
-    if (country) {
-      generateCountryCuriosities(country.name.common).then((res) =>
-        setCountryCuriosities(res.choices[0].message.content),
-      );
-    }
+    handleGenerateCountryCuriosities();
   }, [country]);
 
   return (
@@ -73,9 +105,24 @@ export const CountryScreen = () => {
             </Box>
           </Box>
         </Box>
-        <Box>
-          <Text fontWeight="600">Curiosidades</Text>
-          <Text fontSize="18px">{countryCuriosities}</Text>
+        <Box gap="12px">
+          <Box vCenter flexDirection="row" gap="10px">
+            <Text fontWeight="600">Curiosidades</Text>
+            {completionStatus === 'pending' && (
+              <CircularProgress size={20} thickness={5} />
+            )}
+            {completionStatus === 'succeeded' && (
+              <Chip color="success" label="🤖 Gerado com IA" />
+            )}
+            {completionStatus === 'failed' && <CancelIcon color="error" />}
+          </Box>
+          <Text fontSize="18px">{handleCuriosities()}</Text>
+          {completionStatus === 'succeeded' && (
+            <Text fontSize="14px" color="gray" fontFamily="Titillium Web">
+              IA pode cometer erros. Considere verificar informações
+              importantes.
+            </Text>
+          )}
         </Box>
       </Box>
     </Box>
