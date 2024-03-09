ea0c204df3453cbf9af45639135314a0ed88027e
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 00:45:14 -0300

    feat: improve CountryScreen

--- src/screens/CountryScreen.tsx
+++ src/screens/CountryScreen.tsx
@@ -1,18 +1,75 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { decode } from '@common/utils';
-import { Text } from '@components';
+import { ICountry } from '@common/types';
+import {
+  decode,
+  formatCapitals,
+  formatCurrencies,
+  formatLanguages,
+} from '@common/utils';
+import { Box, Text } from '@components';
+import { Divider } from '@mui/material';
 import { fetchCountry } from '@services/countries';
 
 export const CountryScreen = () => {
   const location = useParams() as { countryName: string };
+  const [country, setCountry] = useState<ICountry>();
 
   useEffect(() => {
     fetchCountry(decode(location.countryName), true).then((res) =>
-      console.log(res.data),
+      setCountry(res.data[0]),
     );
   }, []);
 
-  return <Text>Country Screen!</Text>;
+  return (
+    <Box hCenter vCenter gap="20px" p="2%" height="100vh">
+      <Box gap="12px" width="60%">
+        <Box gap="28px" flexDirection="row">
+          <Box vCenter gap="8px">
+            <Text align="center" component="h1" variant="h1">
+              {country?.name.common}
+            </Text>
+            <img
+              src={country?.flags.png}
+              alt={`Bandeira do país ${country?.name.common}`}
+            />
+          </Box>
+          <Divider orientation="vertical" />
+          <Box gap="12px">
+            <Box>
+              <Text fontWeight="600">Capital</Text>
+              <Text fontSize="24px">
+                {country ? formatCapitals(country) : '-'}
+              </Text>
+            </Box>
+            <Box>
+              <Text fontWeight="600">População</Text>
+              <Text fontSize="24px">{country ? country?.population : '-'}</Text>
+            </Box>
+            <Box>
+              <Text fontWeight="600">Moeda</Text>
+              <Text fontSize="24px">
+                {country ? formatCurrencies(country) : '-'}
+              </Text>
+            </Box>
+            <Box>
+              <Text fontWeight="600">Idioma</Text>
+              <Text fontSize="24px">
+                {country ? formatLanguages(country) : '-'}
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box>
+          <Text fontWeight="600">Fatos</Text>
+          <Text fontSize="24px">...</Text>
+        </Box>
+        <Box>
+          <Text fontWeight="600">Curiosidades</Text>
+          <Text fontSize="24px">...</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
