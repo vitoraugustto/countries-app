6cec807c0402a9e34670435f0411444454ea8a8a
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 10:10:09 -0300

    feat: update styles

--- src/screens/CountriesScreen.tsx
+++ src/screens/CountriesScreen.tsx
@@ -10,6 +10,7 @@ import {
   unaccent,
 } from '@common/utils';
 import { Box, Button, Input, Text } from '@components';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { LinearProgress } from '@mui/material';
 import { DataGrid, GridColDef, GridToolbar, ptBR } from '@mui/x-data-grid';
 import { fetchCountry } from '@services/countries';
@@ -111,72 +112,99 @@ export const CountriesScreen = () => {
             rows={dataGridRows}
           />
         ) : (
-          <Box
-            hCenter
-            gap="28px"
-            flexDirection="row"
-            style={{ flexWrap: 'wrap' }}
-          >
-            {countries.map((country) => (
-              <Box
-                key={country.name.common}
-                p="0px"
-                onClick={() => console.log(country)}
-                borderRadius="8px"
-                style={{
-                  textAlign: 'left',
-                  transition: '.1s',
-                  overflow: 'hidden',
-                  boxShadow:
-                    'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
-                  ':hover': {
-                    transform: 'scale(1.1)',
-                    boxShadow:
-                      'rgba(0, 0, 0, 0.37) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
-                  },
-                }}
-                width="200px"
-                backgroundColor="white"
-              >
-                <Box
-                  vCenter
-                  style={{
-                    maxHeight: '100px',
-                    overflow: 'hidden',
-                    borderBottom: '1px solid black',
-                  }}
-                >
-                  <img
-                    src={country.flags.png}
-                    alt={`Bandeira do país ${country.name.common}`}
-                  />
-                </Box>
-                <Box gap="4px" p="4px">
-                  <Text align="center" fontFamily="Titillium Web">
-                    {country.name.common.toUpperCase()}
-                  </Text>
-                  <Box>
-                    <Text fontSize="12px">Capital</Text>
-                    <Text fontSize="14px">{formatCapitals(country)}</Text>
-                  </Box>
-                  <Box>
-                    <Text fontSize="12px">Moeda:</Text>
-                    <Text fontSize="14px">{formatCurrencies(country)}</Text>
-                  </Box>
-                  <Box>
-                    <Text fontSize="12px">População:</Text>
-                    <Text fontSize="14px">{country.population}</Text>
-                  </Box>
-                  <Box>
-                    <Text fontSize="12px">Idioma:</Text>
-                    <Text fontSize="14px">{formatLanguages(country)}</Text>
-                  </Box>
-                </Box>
-              </Box>
-            ))}
-          </Box>
+          <CountryCards countries={countries} />
         )}
       </Box>
+    </Box>
+  );
+};
+
+const CountryCards: React.FC<{ countries: ICountry[] }> = ({ countries }) => {
+  return (
+    <Box hCenter gap="28px" flexDirection="row" style={{ flexWrap: 'wrap' }}>
+      {countries.length > 0 ? (
+        countries.map((country) => (
+          <Box
+            key={country.name.common}
+            p="0px"
+            onClick={() => console.log(country)}
+            borderRadius="8px"
+            style={{
+              textAlign: 'left',
+              transition: '.1s',
+              overflow: 'hidden',
+              boxShadow:
+                'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
+              ':hover': {
+                transform: 'scale(1.1)',
+                boxShadow:
+                  'rgba(0, 0, 0, 0.37) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
+              },
+            }}
+            width="250px"
+            backgroundColor="white"
+          >
+            <Box
+              vCenter
+              style={{
+                maxHeight: '100px',
+                minHeight: '100px',
+                overflow: 'hidden',
+                borderBottom: '1px solid black',
+              }}
+            >
+              <img
+                src={country.flags.png}
+                alt={`Bandeira do país ${country.name.common}`}
+              />
+            </Box>
+            <Box height="100%" gap="4px" p="8px">
+              <Text align="center" fontFamily="Titillium Web" fontWeight="600">
+                {country.name.common.toUpperCase()}
+              </Text>
+              <Box
+                gap="12px"
+                height="100%"
+                style={{ justifyContent: 'space-between' }}
+              >
+                <CountryInfo title="Capital" text={formatCapitals(country)} />
+                <CountryInfo title="Moeda" text={formatCurrencies(country)} />
+                <CountryInfo title="População" text={country.population} />
+                <CountryInfo title="Idioma" text={formatLanguages(country)} />
+              </Box>
+            </Box>
+          </Box>
+        ))
+      ) : (
+        <Box hCenter>
+          <InfoOutlinedIcon
+            style={{
+              color: 'gray',
+              width: '50px',
+              height: '50px',
+            }}
+          />
+          <Text color="gray" fontFamily="Titillium Web">
+            Nenhum país encontrado.
+          </Text>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+const CountryInfo: React.FC<{ title: string; text: string | number }> = ({
+  title,
+  text,
+}) => {
+  return (
+    <Box>
+      <Text fontSize="12px" letterSpacing={1.6}>
+        {title}
+      </Text>
+      <Text fontSize="14px" letterSpacing={1}>
+        {text}
+      </Text>
     </Box>
   );
 };
