57f13b12696854cc2d24c0db4d72628473c3f0b4
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 01:42:46 -0300

    setup config with new env variables

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,7 @@
 const env = import.meta.env;
 
 export const REST_COUNTRIES_BASE_URL = env.VITE_REST_COUNTRIES_BASE_URL;
+
+export const OPENAI_API_KEY = env.VITE_OPENAI_API_KEY;
+export const OPENAI_CONTEXT = env.VITE_OPENAI_CONTEXT;
+export const OPENAI_REPLACE_KEY = env.VITE_OPENAI_REPLACE_KEY;
