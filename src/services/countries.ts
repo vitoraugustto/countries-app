435e3990e9e7798597a7591e6902a187c1d9a94a
Vitor Augusto <vitoraugusto002@hotmail.com>
2024-03-09 00:44:20 -0300

    feat: add type in fetchCountry

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,9 +1,16 @@
+import { AxiosPromise } from 'axios';
+
+import { ICountry } from '@common/types';
+
 import { instance } from './axios';
 
 export const fetchCountries = () => {
   return instance('/v3.1/all');
 };
 
-export const fetchCountry = (name: string, exact: boolean = false) => {
+export const fetchCountry = (
+  name: string,
+  exact: boolean = false,
+): AxiosPromise<ICountry[]> => {
   return instance(`/v3.1/name/${name}?fullText=${exact}`);
 };
